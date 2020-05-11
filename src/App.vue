41734df3f5cff2b9b223268e8e7d0c214196be2f
Petr Nikitin <nikitablvck@gmai.com>
2020-05-11 10:24:17 +0300

    project templates and routes ready

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <div id="app">
-    <router-view/>
+    <component :is="layout">
+      <router-view/>
+    </component>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+  import EmptyLayout from "./layouts/EmptyLayout";
+  import MainLayout from "./layouts/MainLayout";
 
+  export default {
+    components: {EmptyLayout,MainLayout},
+    computed: {
+      layout() {
+        return (this.$route.meta.layout || 'empty') +'-layout'
+      }
+    }
+  }
+</script>
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.min.css";
+@import "assets/index.css";
 </style>
